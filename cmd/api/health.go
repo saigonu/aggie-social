502e7807ed0c836e107ee7498d5029307e6797f8
Sai Gonuguntla <75554919+saigonu@users.noreply.github.com>
2025-02-16 08:49:18 -0800

    Update work

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -1,9 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	data := map[string]string{
+		"status":  "ok",
+		"env":     app.config.env,
+		"version": version,
+	}
+	if err := writeJSON(w, http.StatusOK, data); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+	}
+}
 
-	w.Write([]byte(`{ "status": "ok" }`))
+func writeJSONError(w http.ResponseWriter, status int, message string) error {
+	type envelope struct {
+		Error string `json:"error"`
+	}
+
+	return writeJSON(w, status, &envelope{Error: message})
 }
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,15 +8,18 @@ import (
 	"github.com/saigonu/aggie-social/internal/store"
 )
 
+const version = "0.0.1"
+
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "localhost:5432"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/aggiesocial?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
 			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
+		env: env.GetString("ENV", "development"),
 	}
 
 	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
@@ -37,4 +40,5 @@ func main() {
 	mux := app.mount()
 
 	log.Fatal(app.run(mux))
+
 }
